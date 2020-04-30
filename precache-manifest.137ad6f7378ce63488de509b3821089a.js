b74ff6a0d77a4cf709857a27e3d9e7f2965d26d7
benjaminlee0 <benjamin_lee@ucsb.edu>
2020-04-29 21:29:24 -0700

    Updates

---
+++ precache-manifest.137ad6f7378ce63488de509b3821089a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09391c886e637f0f621a5122e66e5be5",
+    "url": "/CS185_react/index.html"
+  },
+  {
+    "revision": "a458b9aba0fa8aaf0591",
+    "url": "/CS185_react/static/css/main.06722a2c.chunk.css"
+  },
+  {
+    "revision": "10b3aef64435e42f97b0",
+    "url": "/CS185_react/static/js/2.bc0babed.chunk.js"
+  },
+  {
+    "revision": "04007fcd50e895bcbb2b21327be92c6e",
+    "url": "/CS185_react/static/js/2.bc0babed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a458b9aba0fa8aaf0591",
+    "url": "/CS185_react/static/js/main.d6f49b9f.chunk.js"
+  },
+  {
+    "revision": "88eca6ef2842375ffa1c",
+    "url": "/CS185_react/static/js/runtime-main.5e804972.js"
+  },
+  {
+    "revision": "123016ff2229e83cb9e29ff92f06b1e6",
+    "url": "/CS185_react/static/media/aboutpic.123016ff.PNG"
+  },
+  {
+    "revision": "d693e4a51e42bc1111d2f2015bb3aa39",
+    "url": "/CS185_react/static/media/coffee1.d693e4a5.jpg"
+  },
+  {
+    "revision": "1160c803deb6eb9bdc0eb588ff8b8368",
+    "url": "/CS185_react/static/media/coffee2.1160c803.jpg"
+  },
+  {
+    "revision": "5b411a50535526500a1ce2dd8a40c9f1",
+    "url": "/CS185_react/static/media/matcha1.5b411a50.jpg"
+  },
+  {
+    "revision": "dce15c4224a2787e394149463c7c84b2",
+    "url": "/CS185_react/static/media/matcha2.dce15c42.jpg"
+  },
+  {
+    "revision": "d1dfe1e8bfbf279948d8d6d962824163",
+    "url": "/CS185_react/static/media/profile.d1dfe1e8.jpg"
+  },
+  {
+    "revision": "d7c05b05f6999960c6d0607d6688b997",
+    "url": "/CS185_react/static/media/rideshare.d7c05b05.PNG"
+  }
+]);
