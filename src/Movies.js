04463b60be4ebf93df7d8bb8ae7275c157c0c10f
benjaminlee0 <benjamin_lee@ucsb.edu>
2020-05-13 16:49:07 -0700

    fixed http error

--- src/Movies.js
+++ src/Movies.js
@@ -17,36 +17,36 @@ export class Movies extends Component{
     componentDidMount(){
         console.log("in mount");
         var data = [];
-        axios.get('http://www.omdbapi.com/?i=tt0816692&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt0816692&apikey=64551cfe').then(res => {
             const interstellar = res.data;
             data.push(interstellar);
             console.log(interstellar);
         })
-        axios.get('http://www.omdbapi.com/?i=tt3460252&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt3460252&apikey=64551cfe').then(res => {
             const hatefuleight = res.data;
             data.push(hatefuleight);
         })
-        axios.get('http://www.omdbapi.com/?i=tt7131622&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt7131622&apikey=64551cfe').then(res => {
             const hollywood = res.data;
             data.push(hollywood);
         })
-        axios.get('http://www.omdbapi.com/?i=tt0468569&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt0468569&apikey=64551cfe').then(res => {
             const darkknight = res.data;
             data.push(darkknight);
         })
-        axios.get('http://www.omdbapi.com/?i=tt0080684&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt0080684&apikey=64551cfe').then(res => {
             const empire = res.data;
             data.push(empire);
         })
-        axios.get('http://www.omdbapi.com/?i=tt1375666&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt1375666&apikey=64551cfe').then(res => {
             const inception = res.data;
             data.push(inception);
         })
-        axios.get('http://www.omdbapi.com/?i=tt0075314&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt0075314&apikey=64551cfe').then(res => {
             const taxi = res.data;
             data.push(taxi);
         })
-        axios.get('http://www.omdbapi.com/?i=tt0032599&apikey=64551cfe').then(res => {
+        axios.get('https://www.omdbapi.com/?i=tt0032599&apikey=64551cfe').then(res => {
             const friday = res.data;
             data.push(friday);
         })
@@ -60,21 +60,19 @@ export class Movies extends Component{
         console.log("before movie list");
         console.log(this.state.movieArray);
         var movieList = this.state.movieArray.map((value, index)=>{
-        var title = value.Title;
-        console.log("in movie list");
-        var director = value.Director;
-        var imdb = value.imdbRating;
-        var poster = value.Poster;
-        var caption = (title + "\n" + "Director: " + director + "\n" + "IMDB Rating: " + imdb);
+            var title = value.Title;
+            console.log("in movie list");
+            var director = value.Director;
+            var imdb = value.imdbRating;
+            var poster = value.Poster;
+            var caption = (title + "\n" + "Director: " + director + "\n" + "IMDB Rating: " + imdb);
 
-        return(
-            <div className = "MovieItem">
-                    <img key={index} src={poster} alt={caption} className="MovieImg"></img>
-            </div>
-        )
-    })
-
-    console.log("rendered");
+            return(
+                <div className = "MovieItem">
+                        <img key={index} src={poster} alt={caption} className="MovieImg"></img>
+                </div>
+            )
+        })
 
     return (
         <SRLWrapper>
