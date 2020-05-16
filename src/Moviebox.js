cb935e26b21da700d34908e36fbbb738b591629d
benjaminlee0 <benjamin_lee@ucsb.edu>
2020-05-15 23:56:59 -0700

    completed assignment 6

---
+++ src/Moviebox.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import { SRLWrapper } from "simple-react-lightbox";
+import './css/Moviebox.css';
+import disableScroll from 'disable-scroll';
+
+export class Moviebox extends Component{
+
+    constructor(props){
+
+        super(props);
+        this.state = {
+            poster: this.props.poster,
+            title: this.props.title,
+            director: this.props.director,
+            rating: this.props.rating
+        }
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentWillUnmount = this.componentWillUnmount.bind(this);
+        this.clickClose = this.clickClose.bind(this);
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+    }
+
+    componentDidMount(){
+        // if(this.wrapperRef){
+        //     document.body.style.overflow = 'hidden';
+        // }
+        disableScroll.on();
+        document.addEventListener('mousedown', this.clickClose);
+        console.log("listener added");
+    }
+
+    componentWillUnmount(){
+        document.removeEventListener('mousedown', this.clickClose);
+        //document.body.style.overflow = '';
+        disableScroll.off();
+    }
+
+    setWrapperRef(node){
+        this.wrapperRef = node;
+    }
+
+    clickClose(event){
+        if(this.wrapperRef &&!this.wrapperRef.contains(event.target)){
+            this.props.handleClose();
+            console.log("handling close");
+        }
+    }
+
+
+    render(){
+
+    return (
+        <div className = "Modal" id="Modal">
+            <div className = "Content" ref={this.setWrapperRef}>
+                <div className="Poster">
+                    <img src={this.state.poster} className="MovieboxImg" id="poster"></img>
+                </div>
+                <div className = "Text">
+                <h1 className="Title">{this.state.title}</h1>
+                <h2 className="Director">{this.state.director}</h2>
+                    <p className="Rating">{this.state.rating} on IMDB</p>
+                    </div>
+            </div>
+        </div>
+
+    );
+  }
+}
+export default Moviebox;
--- src/Movies.js
+++ src/Movies.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import { SRLWrapper } from "simple-react-lightbox";
+import Moviebox from './Moviebox';
+import ReactDOM from 'react-dom';
+import './css/Movies.css';
 
 export class Movies extends Component{
 
@@ -8,80 +11,100 @@ export class Movies extends Component{
 
         super(props);
         this.state = {
+            opt: {
+                thumbnails: {
+                    showThumbnails: "false"
+                },
+                settings: {
+                    disableWheelControls: true
+                },
+                caption: {
+                    captionColor: "red"
+                }
+            },
+            added: "false",
+            lightbox: '',
             movieArray: []
         }
 
         this.componentDidMount = this.componentDidMount.bind(this);
+        this.addLightbox = this.addLightbox.bind(this);
+        this.removeLightbox = this.removeLightbox.bind(this);
     }
 
     componentDidMount(){
-        console.log("in mount");
         var data = [];
-        axios.get('http://www.omdbapi.com/?i=tt0816692&apikey=64551cfe').then(res => {
-            const interstellar = res.data;
-            data.push(interstellar);
-            console.log(interstellar);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt3460252&apikey=64551cfe').then(res => {
-            const hatefuleight = res.data;
-            data.push(hatefuleight);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt7131622&apikey=64551cfe').then(res => {
-            const hollywood = res.data;
-            data.push(hollywood);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt0468569&apikey=64551cfe').then(res => {
-            const darkknight = res.data;
-            data.push(darkknight);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt0080684&apikey=64551cfe').then(res => {
-            const empire = res.data;
-            data.push(empire);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt1375666&apikey=64551cfe').then(res => {
-            const inception = res.data;
-            data.push(inception);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt0075314&apikey=64551cfe').then(res => {
-            const taxi = res.data;
-            data.push(taxi);
-        })
-        axios.get('http://www.omdbapi.com/?i=tt0032599&apikey=64551cfe').then(res => {
-            const friday = res.data;
-            data.push(friday);
-        })
+        const r0 = axios.get('http://www.omdbapi.com/?i=tt0816692&apikey=64551cfe');
+        const r1 = axios.get('http://www.omdbapi.com/?i=tt3460252&apikey=64551cfe');
+        const r2 = axios.get('http://www.omdbapi.com/?i=tt7131622&apikey=64551cfe');
+        const r3 = axios.get('http://www.omdbapi.com/?i=tt0468569&apikey=64551cfe');
+        const r4 = axios.get('http://www.omdbapi.com/?i=tt0080684&apikey=64551cfe');
+        const r5 = axios.get('http://www.omdbapi.com/?i=tt1375666&apikey=64551cfe');
+        const r6 = axios.get('http://www.omdbapi.com/?i=tt0075314&apikey=64551cfe');
+        const r7 = axios.get('http://www.omdbapi.com/?i=tt0032599&apikey=64551cfe');
+
+        const begin = this;
+
+        axios.all([r0,r1,r2,r3,r4,r5,r6,r7]).then(axios.spread((...responses)=>{
+            responses.forEach(function(item){
+                data.push(item.data);
+                begin.setState({
+                    added: "true"
+                })
+            })
+        }))
+        
         this.setState({
             movieArray: data
         })
     }
 
+    addLightbox(title, director, poster, rating){
+        console.log("in addlightbox");
+        var newbox = React.createElement(
+            Moviebox,
+            {
+                title: title,
+                director: director,
+                poster: poster,
+                rating: rating,
+                handleClose: this.removeLightbox
+            }
+        )
+        var bod = document.getElementById("Lightbox");
+        ReactDOM.render(newbox, bod);
+        this.setState({
+            lightbox: newbox
+        })
+    }
+
+    removeLightbox(){
+        console.log("in remove ligthobx");
+        ReactDOM.unmountComponentAtNode(document.getElementById("Lightbox"));
+    }
 
     render(){
-        console.log("before movie list");
-        console.log(this.state.movieArray);
         var movieList = this.state.movieArray.map((value, index)=>{
         var title = value.Title;
-        console.log("in movie list");
         var director = value.Director;
         var imdb = value.imdbRating;
         var poster = value.Poster;
-        var caption = (title + "\n" + "Director: " + director + "\n" + "IMDB Rating: " + imdb);
-
         return(
             <div className = "MovieItem">
-                    <img key={index} src={poster} alt={caption} className="MovieImg"></img>
+                    <img key={index} src={poster} alt={title} className="MovieImg" onClick={()=>{this.addLightbox(title, director, poster, imdb)}}></img>
             </div>
         )
     })
 
-    console.log("rendered");
-
     return (
-        <SRLWrapper>
+        <div>
+        <div className="Lightbox" id="Lightbox">
+
+        </div>
         <div className = "MovieDiv">
             {movieList}
         </div>
-        </SRLWrapper>
+        </div>
     );
   }
 }
