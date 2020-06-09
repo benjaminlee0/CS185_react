bd2760a9f98ec0bd6360bb038fdb6d4c0b0f1c7f
benjaminlee0 <benjamin_lee@ucsb.edu>
2020-06-08 23:06:16 -0700

    Updates

---
+++ precache-manifest.e1133b5d9de8c584ae3b70feb5cd377a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8290452e25a5331fa1a24b126645640e",
+    "url": "/CS185_react/index.html"
+  },
+  {
+    "revision": "735c6c6a179de289dbc6",
+    "url": "/CS185_react/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "7bd9b46ff988af0e9c25",
+    "url": "/CS185_react/static/css/main.113e44f8.chunk.css"
+  },
+  {
+    "revision": "735c6c6a179de289dbc6",
+    "url": "/CS185_react/static/js/2.b0031f20.chunk.js"
+  },
+  {
+    "revision": "441492c9a5cc96d03288ed2f0b00ec1c",
+    "url": "/CS185_react/static/js/2.b0031f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd9b46ff988af0e9c25",
+    "url": "/CS185_react/static/js/main.0a73d954.chunk.js"
+  },
+  {
+    "revision": "88eca6ef2842375ffa1c",
+    "url": "/CS185_react/static/js/runtime-main.5e804972.js"
+  },
+  {
+    "revision": "123016ff2229e83cb9e29ff92f06b1e6",
+    "url": "/CS185_react/static/media/aboutpic.123016ff.PNG"
+  },
+  {
+    "revision": "d693e4a51e42bc1111d2f2015bb3aa39",
+    "url": "/CS185_react/static/media/coffee1.d693e4a5.jpg"
+  },
+  {
+    "revision": "1160c803deb6eb9bdc0eb588ff8b8368",
+    "url": "/CS185_react/static/media/coffee2.1160c803.jpg"
+  },
+  {
+    "revision": "5b411a50535526500a1ce2dd8a40c9f1",
+    "url": "/CS185_react/static/media/matcha1.5b411a50.jpg"
+  },
+  {
+    "revision": "dce15c4224a2787e394149463c7c84b2",
+    "url": "/CS185_react/static/media/matcha2.dce15c42.jpg"
+  },
+  {
+    "revision": "d1dfe1e8bfbf279948d8d6d962824163",
+    "url": "/CS185_react/static/media/profile.d1dfe1e8.jpg"
+  },
+  {
+    "revision": "d7c05b05f6999960c6d0607d6688b997",
+    "url": "/CS185_react/static/media/rideshare.d7c05b05.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CS185_react/precache-manifest.498373889c7b4b618c4f9168181995e1.js"
+  "/CS185_react/precache-manifest.e1133b5d9de8c584ae3b70feb5cd377a.js"
 );
 
 self.addEventListener('message', (event) => {
